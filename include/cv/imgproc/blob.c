fb6586f970ae0ca133f6dcc2b0dcbf356fc83433
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-04-02 16:30:48 +0530

    fixed labelled connected components algorithm; fixed bug in pass2; added flood_row() function

--- include/cv/imgproc/blob.c
+++ include/cv/imgproc/blob.c
@@ -1,12 +1,27 @@
 #include "blob.h"
 
+/* flood fill a row of pixels */
+static void floodrow(const MAT *m, MAT *res, int i, int j, double label)
+{
+	int k=j;
+	/* fil starting pixel */
+	MSET(res, i, j, label);
+	/* fill left pixels */
+	while ((--k > 0) && (MGET(m, i, k) == MGET(m, i, k+1)))
+		MSET(res, i, k, label);
+	/* fill right pixels */
+	k = j;
+	while ((++k < m->size2) && (MGET(m, i, k) == MGET(m, i, k-1)))
+		MSET(res, i, k, label);
+}
+
 /* labelled connected components algorithm */
-MAT *cv_conncomp(const MAT *const m, int bg)
+MAT *cv_conncomp(const MAT *m, int bg)
 {
 	if (!m)
 		cv_error("cv_conncomp: source matrix not provided!\n");
 	int i, j, cnt=1;
-	double label1, label2, pixel=0;
+	double label, pixel=0;
 	MAT *res = gsl_matrix_calloc(m->size1, m->size2);
 
 	/* pass 1 */
@@ -26,18 +41,15 @@ MAT *cv_conncomp(const MAT *const m, int bg)
 	}
 
 	/* pass 2 */
-	for (j= m->size2-2; j>0; --j) {
-		for (i= m->size1-2; i>0; --i) {
+	for (i=m->size1-2; i>0; --i) {
+		for (j=m->size2-2; j>0; --j) {
 			pixel = MGET(m, i, j);
 			if (!bg && !pixel)
 				continue;
-			label1 = MGET(res, i, j);
-			if ((label1 != (label2 = MGET(res, i+1, j))) &&
+			if ((MGET(res, i, j) !=
+				(label = MGET(res, i+1, j))) &&
 				(pixel == MGET(m, i+1, j))) {
-				MSET(res, i, j, label2);
-			} else if ((label1 != (label2 = MGET(res, i, j+1))) &&
-				(pixel == MGET(m, i, j+1))) {
-				MSET(res, i, j, label2);
+				floodrow(m, res, i, j, label);
 			}
 		}
 	}
