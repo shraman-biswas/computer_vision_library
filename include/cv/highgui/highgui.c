c0116456ee0f1890b2a6894e07d422d324b43a43
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-04-02 22:51:16 +0530

    fixed cv_imshow() bug; now accepts any arbitrary matrix

--- include/cv/highgui/highgui.c
+++ include/cv/highgui/highgui.c
@@ -1,33 +1,32 @@
 #include "highgui.h"
 
-/* TODO: fix bug - error if m is non-squre */
 /* show image matrix */
 void cv_imshow(const char *str, const MAT *m)
 {
 	double tmp[m->size1][m->size2], **data = (double **)tmp;
+	PLFLT w = m->size1, h = m->size2;
 
-	/* allocate image memory and convert to 2D array */
-	plAlloc2dGrid(&data, m->size2, m->size1);
+	/* allocate image memory and convert matrix to 2d array */
+	plAlloc2dGrid(&data, w, h);
 	cv_to2d(m, data);
 
 	/* initialize plplot */
 	plsetopt("plwindow", str);
-	plsetopt("geometry", "500x500");
-	plsdev("xwin");
-	plinit();
+	plsetopt("geometry", "400x400");
+	plsetopt("ori", "1");
+	plstart("xwin", 1, 1);
 	pladv(0);
-	plvpas(0, 1, 0, 1, m->size1 / m->size2);
-	plwind(0, m->size2, 0, m->size1);
-	/* plenv(0, m->size2, 0, m->size1, 1, -2); */
+	plvpas(0, 1, 0, 1, h / w);
+	plwind(0, w, 0, h);
 
 	/* plot image */
-	plimage((const PLFLT **)data,
-		m->size2, m->size1,
-		0, m->size2, 0, m->size1,
-		0, 0,
-		0, m->size2, 0, m->size1);
+	plimage((const PLFLT **)data,	/* idata */
+		w, h,			/* nx, ny */
+		0, w, 0, h,		/* xmin, xmax, ymin, ymax */
+		0, 0,			/* zmin, zmax */
+		0, w, 0, h);		/* Dxmin, Dxmax, Dymin, Dymax */
 
 	/* free image memory and end plplot */
-	plFree2dGrid(data, m->size2, m->size1);
+	plFree2dGrid(data, w, h);
 	plend();
 }
